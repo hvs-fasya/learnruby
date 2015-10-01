ab024b4cfb72ed1fbaaacbe336de0c68f5e1f0db
hvs-fasya <hvs-fasya@mail.ru>
2015-10-01 15:01:48 +0300

    tasks 1 and 2 for lesson 4 Изменения для закрепления: 	modified:   .gitignore 	new file:   lesson4/excersize_01.rb 	new file:   lesson4/excersize_02.rb

---
+++ lesson4/excersize_01.rb
@@ -0,0 +1,37 @@
+puts "Введите число"
+x = gets.to_i
+x > 1 ? interval = (1..x) : interval = (x..1)
+delimeter = interval.count
+
+#для for
+sum_for = 0
+for value in interval do
+  sum_for += value
+end
+
+#для while
+el = 1
+sum_while = 0
+x > 1 ? step = 1 : step = -1
+while interval.include?(el) do
+  sum_while += el
+  el += step
+end
+
+#для until
+el = 1
+sum_until = 0
+
+until !interval.include?(el) do
+  sum_until += el
+  el += step
+end
+
+#для each
+sum_each = 0
+interval.each{|el| sum_each += el}
+
+puts "Сумма элементов по for: #{sum_for}; ср/арифм: #{sum_for.to_f / delimeter}"
+puts "Сумма элементов по while: #{sum_while}; ср/фрифм: #{sum_while.to_f / delimeter}"
+puts "Сумма элементов по until: #{sum_until}; ср/фрифм: #{sum_until.to_f / delimeter}"
+puts "Сумма элементов по each: #{sum_each}; ср/фрифм: #{sum_each.to_f / delimeter}"
---
+++ lesson4/excersize_02.rb
@@ -0,0 +1,10 @@
+puts "Введите целое число"
+x = gets.to_i
+x > 1 ? interval = (1..x) : interval = (x..1)
+delimeter = interval.count / 2
+
+#для each
+sum_each = 0
+interval.each{|el| el.even? ? sum_each += el : sum_each = sum_each}
+
+puts "Сумма четных элементов по each: #{sum_each}; ср/арифм: #{sum_each.to_f / delimeter}"
